6d4d87416145a1b474f5029ab17923630301ff97
Beebane25 <zulfajrisurya30@gmail.com>
2024-12-10 23:05:34 +0700

    four commit

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('menu/<int:menu_id>/', views.menu_detail, name='menu_detail'),
     path('home/', views.home, name='home_logged_in'), 
-    path('home/profile', views.Profile, name='profile'),
+    path('home/profile/', views.Profile, name='profile'),
 ]
